6ae70ffdc45135a3acffea8653732c0fa5bdfaee
cyrusteo001 <51070670+cyrusteo001@users.noreply.github.com>
2020-03-17 14:59:38 +0800

    Update Inventory.java

--- Karat/app/src/main/java/com/example/karat/inventory/Inventory.java
+++ Karat/app/src/main/java/com/example/karat/inventory/Inventory.java
@@ -4,109 +4,27 @@ import java.util.ArrayList;
 
 class Inventory{
     private static Inventory single_instance = null;
-    public static ArrayList<Listing> meatInventory = new ArrayList<>();
-    public static ArrayList<Listing> vegInventory = new ArrayList<>();
-    public static ArrayList<Listing> fruitsInventory = new ArrayList<>();
-    public static ArrayList<Listing> seafoodInventory = new ArrayList<>();
-    public static ArrayList<Listing> cannedInventory = new ArrayList<>();
+    public static ArrayList<Listing> Inventory = new ArrayList<>();
 
     private Inventory() {}
     
-    public void addMeat(double price, double discount, String name) {
-        meatInventory.add(new Listing(price, discount, name, "meats"));
+    public void addListing(double price, double discount, String name, String category) {
+        Inventory.add(new Listing(price, discount, name, category));
     }
 
-    public void addVeg(double price, double discount, String name) {
-        vegInventory.add(new Listing(price, discount, name, "vegetables"));
-    }
-
-    public void addFruits(double price, double discount, String name) {
-        fruitsInventory.add(new Listing(price, discount, name, "fruits"));
-    }
-
-    public void addSeafood(double price, double discount, String name) {
-        seafoodInventory.add(new Listing(price, discount, name, "seafood"));
-    }
-
-    public void addCanned(double price, double discount, String name) {
-        cannedInventory.add(new Listing(price, discount, name, "canned"));
-    }
-
-    public int purchase(int listingId, String category){
-        if (category == "meats"){
-            for (Listing l: meatInventory) {
-                if (listingId == l.getListingId()){
-                    if (l.available == true){
-                        l.quantity--;
-                        if (l.quantity == 0) {
-                            l.setListingAvailable(false)
-                        }
-                        return 1;
+    public int purchase(int listingId){
+        for (Listing l: Inventory) {
+            if (listingId == l.getListingId()){
+                if (l.available == true){
+                    l.quantity--;
+                    if (l.quantity == 0) {
+                        l.setListingAvailable(false)
                     }
+                    return 1;
                 }
-            }    
-            return 0;
-        }
-
-        else if (category == "vegetables"){
-            for (Listing l: vegInventory) {
-                if (listingId == l.getListingId()){
-                    if (l.available == true){
-                        l.quantity--;
-                        if (l.quantity == 0) {
-                            l.setListingAvailable(false)
-                        }
-                        return 1;
-                    }
-                }
-            }    
-            return 0;
-        }
-
-        else if (category == "fruits"){
-            for (Listing l: fruitsInventory) {
-                if (listingId == l.getListingId()){
-                    if (l.available == true){
-                        l.quantity--;
-                        if (l.quantity == 0) {
-                            l.setListingAvailable(false)
-                        }
-                        return 1;
-                    }
-                }
-            }    
-            return 0;
-        }
-
-        else if (category == "seafood"){
-            for (Listing l: seafoodInventory) {
-                if (listingId == l.getListingId()){
-                    if (l.available == true){
-                        l.quantity--;
-                        if (l.quantity == 0) {
-                            l.setListingAvailable(false)
-                        }
-                        return 1;
-                    }
-                }
-            }    
-            return 0;
-        }
-
-        else if (category == "canned"){
-            for (Listing l: cannedInventory) {
-                if (listingId == l.getListingId()){
-                    if (l.available == true){
-                        l.quantity--;
-                        if (l.quantity == 0) {
-                            l.setListingAvailable(false)
-                        }
-                        return 1;
-                    }
-                }
-            }    
-            return 0;
-        }
+            }
+        }    
+        return 0;
     }
 
     public static Inventory getInstance(){
