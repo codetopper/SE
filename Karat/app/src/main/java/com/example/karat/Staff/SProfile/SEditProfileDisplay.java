9e08ded765b8ec36e6ba1e7d0bee763dc3e1af35
sailesht <sailesh9397@gmail.com>
2020-03-19 20:14:00 +0800

    Refine UI Profile Page

---
+++ Karat/app/src/main/java/com/example/karat/Staff/SProfile/SEditProfileDisplay.java
@@ -0,0 +1,18 @@
+package com.example.karat.Staff.SProfile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.karat.R;
+
+import static com.example.karat.R.*;
+
+public class SEditProfileDisplay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(layout.activity_s_edit_profile_display);
+    }
+}
--- Karat/app/src/main/java/com/example/karat/Staff/SProfile/SProfileDisplay.java
+++ Karat/app/src/main/java/com/example/karat/Staff/SProfile/SProfileDisplay.java
@@ -6,10 +6,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.karat.Customer.CHome.CHomeDisplay;
 import com.example.karat.Customer.COrder.COrderDisplay;
+import com.example.karat.Customer.CProfile.CEditProfileDisplay;
 import com.example.karat.Customer.CProfile.CProfileDisplay;
+import com.example.karat.Login.LoginDisplay;
+import com.example.karat.Login.ResetPwDisplay;
 import com.example.karat.R;
 import com.example.karat.Staff.SHome.SHomeDisplay;
 import com.example.karat.Staff.SOrder.SOrderDisplay;
@@ -17,10 +22,44 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class SProfileDisplay extends AppCompatActivity {
 
+    private Button logOffBtn, changePwBtn, editProfileBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_s_profile_display);
+
+        initialiseUI();
+
+        //Log off Button
+        logOffBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent logOffIntent = new Intent(getApplicationContext(), LoginDisplay.class);
+                startActivity(logOffIntent);
+                overridePendingTransition(0,0);
+            }
+        });
+
+        //ChangePw Button
+        changePwBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent changePwIntent = new Intent(getApplicationContext(), ResetPwDisplay.class);
+                startActivity(changePwIntent);
+                overridePendingTransition(0,0);
+            }
+        });
+
+        //EditProfile Button
+        editProfileBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent editProfileIntent = new Intent(getApplicationContext(), CEditProfileDisplay.class);
+                startActivity(editProfileIntent);
+                overridePendingTransition(0,0);
+            }
+        });
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navi);
 
@@ -46,5 +85,10 @@ public class SProfileDisplay extends AppCompatActivity {
                 return false;
             }
         });
+    }
+    private void initialiseUI(){
+        logOffBtn = findViewById(R.id.logOff);
+        changePwBtn = findViewById(R.id.changePw);
+        editProfileBtn = findViewById(R.id.editProfile);
     }
 }
