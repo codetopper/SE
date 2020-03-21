85bfef7985da7c7baab2757c6532f86b6173b0fa
sailesht <sailesh9397@gmail.com>
2020-03-21 22:48:36 +0800

    Revert "Revert "Merge pull request #16 from codetopper/mine""
    This reverts commit 3a62e8d00db70cdfcc2a5ee806840cda6d05001c.

--- Karat/app/src/main/java/com/example/karat/Staff/SHome/SHomeDisplay.java
+++ Karat/app/src/main/java/com/example/karat/Staff/SHome/SHomeDisplay.java
@@ -6,6 +6,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.karat.Customer.CHome.CHomeDisplay;
 import com.example.karat.Customer.COrder.COrderDisplay;
@@ -14,13 +18,36 @@ import com.example.karat.R;
 import com.example.karat.Staff.SOrder.SOrderDisplay;
 import com.example.karat.Staff.SProfile.SProfileDisplay;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
 public class SHomeDisplay extends AppCompatActivity {
 
+    private TextView nameTV, addressTV, timeTV;
+    private FirebaseAuth mAuth;
+    private DatabaseReference mDatabase;
+    private Button addListingBtn;
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_s_home_display);
+
+        initialiseUI();
+        setHeader();
+
+        addListingBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent smt = new Intent(getApplicationContext(), manageListingDisplay.class);
+                startActivity(smt);
+            }
+        });
+
+
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navi);
 
@@ -46,5 +73,35 @@ public class SHomeDisplay extends AppCompatActivity {
                 return false;
             }
         });
+    }
+
+    private void setHeader(){
+        final String email = mAuth.getCurrentUser().getEmail().replace("@", "")
+                .replace(".", "");
+
+        mDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String name = dataSnapshot.child("UserDatabase").child(email).child("firstName").getValue(String.class) +
+                        dataSnapshot.child("UserDatabase").child(email).child("lastName").getValue(String.class);
+                String address = dataSnapshot.child("UserDatabase").child(email).child("address").getValue(String.class);
+                String timeStart = dataSnapshot.child("UserDatabase").child(email).child("openingHour").getValue(String.class);
+                String timeEnd = dataSnapshot.child("UserDatabase").child(email).child("closingHour").getValue(String.class);
+                nameTV.setText(name);
+                addressTV.setText(address);
+                timeTV.setText(timeStart+"-"+timeEnd);
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(getApplicationContext(), databaseError.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void initialiseUI(){
+        addListingBtn = findViewById(R.id.addListing);
+        nameTV = findViewById(R.id.nameTV);
+        addressTV = findViewById(R.id.addressTV);
+        timeTV = findViewById(R.id.timeTV);
     }
 }
