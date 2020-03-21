31538f2ef36fffa72ce5715f29f9c85d23488c4e
tanderry <44270571+tanderry@users.noreply.github.com>
2020-03-21 18:06:04 +0800

    Revert "Revert "Merge pull request #14 from codetopper/Derry""
    This reverts commit 42468ee2e82ab1e8367880e2b13877553a2b7870.

--- Karat/app/src/main/java/com/example/karat/Customer/Cart/CartDisplay.java
+++ Karat/app/src/main/java/com/example/karat/Customer/Cart/CartDisplay.java
@@ -2,23 +2,70 @@ package com.example.karat.Customer.Cart;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.karat.Customer.CHome.CHomeDisplay;
 import com.example.karat.Customer.COrder.COrderDisplay;
 import com.example.karat.Customer.CProfile.CProfileDisplay;
+import com.example.karat.Customer.Cart.*;
 import com.example.karat.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
 public class CartDisplay extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private TextView subtotal;
+    private TextView GST;
+
+    CartManager cartManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart_display);
+
+        /* Initialise UI */
+
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.RecyclerView2);
+        cartManager= new CartManager();
+
+        cartManager.total.addtoCart(3.5,"Chicken",3,R.drawable.ic_person);
+        cartManager.total.addtoCart(4.5,"Duck",3,R.drawable.ic_history);
+        cartManager.total.addtoCart( 5.5,"Rice",3,R.drawable.ic_person);
+
+
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mAdapter = new CartAdapter(cartManager.total.getCartlist());
+
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
+
+
+        subtotal = (TextView) findViewById(R.id.subTotal);
+        GST = (TextView) findViewById(R.id.GST);
+
+
+        subtotal.setText(String.valueOf(cartManager.subtotal()));
+        GST.setText(String.valueOf(cartManager.gst()));
+
+        /* Buttons for Add/Cart */
+
+        /* Bottom Navigation Menu */
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navi);
 
@@ -45,9 +92,7 @@ public class CartDisplay extends AppCompatActivity {
             }
         });
 
+
+
     }
-
-
-
-
 }
