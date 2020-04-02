8e52cf4b48a91508c3a8ca2ff96ed470533d2b11
ZY <teezhengyang16@gmail.com>
2020-04-02 15:10:05 +0800

    test expandable

--- Karat/app/src/main/java/com/example/karat/Customer/COrder/CustomerOrders.java
+++ Karat/app/src/main/java/com/example/karat/Customer/COrder/CustomerOrders.java
@@ -5,11 +5,6 @@ import java.util.ArrayList;
 import static java.lang.reflect.Array.newInstance;
 
 public class CustomerOrders {
-    protected static int orderQuantity = 0;
     public static ArrayList<Receipt> orders = new ArrayList<>();
-
-    public static void purchase(int itemId, String name, int custId, int quantity, double price, String date, int supId, String supName) {
-        orders.add(new Receipt(itemId, name, custId, quantity, price, date, supId, supName));
-        orderQuantity++;
-    }
+    
 }
