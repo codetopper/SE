c3a66bb3f51853d6950efec4ce3c2858af4d5de7
sailesht <sailesh9397@gmail.com>
2020-04-02 16:10:45 +0800

    Revert "Merge branch 'master' into sgt"
    This reverts commit 3f29a7f32594a946ba17e629a5a8ee8cfd1870ce, reversing
    changes made to d34b0ecc17c0faa18e99b5bf1a4647ef4382e378.

--- Karat/app/src/main/java/com/example/karat/Customer/CHome/StaggeredRecyclerViewAdapter.java
+++ Karat/app/src/main/java/com/example/karat/Customer/CHome/StaggeredRecyclerViewAdapter.java
@@ -1,7 +1,6 @@
 package com.example.karat.Customer.CHome;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.media.Image;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -18,7 +17,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
@@ -40,7 +38,7 @@ import java.util.List;
 public class StaggeredRecyclerViewAdapter extends RecyclerView.Adapter<StaggeredRecyclerViewAdapter.Viewholder>{
 
     private static final String TAG = "StaggeredRecyclerViewAd";
-    private ArrayList<Listing> Listing;
+
     private ArrayList<String> mNames = new ArrayList<>();
     private ArrayList<String> mImageUrls = new ArrayList<>();
     private ArrayList<String> mDescription = new ArrayList<>();
@@ -49,42 +47,21 @@ public class StaggeredRecyclerViewAdapter extends RecyclerView.Adapter<Staggered
     private static ArrayList<Integer> mListingId = new ArrayList<>();
     private static ArrayList<Integer> mQty = new ArrayList<>();
     private static ArrayList<Double> mPrice = new ArrayList<>();
-    private FirebaseDatabase firebaseDatabase;
+    private FirebaseDatabase firebaseDB;
     private DatabaseReference mDatabase;
     private FirebaseAuth mAuth;
-
     private Context mContext;
 
     public StaggeredRecyclerViewAdapter(Context context){
         mContext = context;
     }
 
-    public StaggeredRecyclerViewAdapter(Context context,
-                                        ArrayList<Listing> Listing
-                                        //ArrayList<String> names, ArrayList<String> imageUrls
-    ){
-
-        for(Listing listing: Listing) {
-            mNames.add(listing.getListingName());
-            mImageUrls.add(listing.getImage_url());
-            mListingId.add(listing.getListingId());
-            mQty.add(Integer.parseInt(listing.getListingQuantity()+""));
-        }
-        mContext = context;
-
-        /*mNames = names;
-        mImageUrls = imageUrls;
-        mContext = context;*/
-    }
-
-
     public void reset(ArrayList<Listing> listing){
-        Listing = (ArrayList<com.example.karat.inventory.Listing>) listing.clone();
+
         mNames.clear();
         mImageUrls.clear();
         mListingId.clear();
         mQty.clear();
-
         mPrice.clear();
         mDescription.clear();
         mDiscount.clear();
@@ -110,8 +87,8 @@ public class StaggeredRecyclerViewAdapter extends RecyclerView.Adapter<Staggered
     @Override
     public void onBindViewHolder(@NonNull final Viewholder holder, final int position) {
 
-        firebaseDatabase = FirebaseDatabase.getInstance();
-        mDatabase = firebaseDatabase.getReference();
+        firebaseDB = FirebaseDatabase.getInstance();
+        mDatabase = firebaseDB.getReference();
         mAuth = FirebaseAuth.getInstance();
 
         RequestOptions requestOptions = new RequestOptions();
@@ -142,8 +119,6 @@ public class StaggeredRecyclerViewAdapter extends RecyclerView.Adapter<Staggered
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
                 holder.minus.setEnabled(true);
                 holder.plus.setEnabled(true);
-                holder.addtoCart.setEnabled(true);
-                holder.addtoCart.setBackgroundColor(0xFFBDE0B7);
             }
 
             @Override
@@ -208,22 +183,12 @@ public class StaggeredRecyclerViewAdapter extends RecyclerView.Adapter<Staggered
         holder.addtoCart.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                String text = holder.homequantity.getText().toString();
-                if (Integer.parseInt(text) == 0) {
-                    holder.addtoCart.setEnabled(false);
-                    holder.addtoCart.setBackgroundColor(Color.GRAY);
-                    Toast.makeText(mContext, "Please add at least one value to cart", Toast.LENGTH_SHORT).show();
-                }
-                else {
-                    String email = mAuth.getCurrentUser().getEmail().replace("@", "")
-                            .replace(".", "");
-                    //Toast.makeText(mContext, Listing.get(position).getListingName(), Toast.LENGTH_LONG).show();
-                    int id = mListingId.get(position);
-                    mDatabase.child("UserCart").child(email).child(id + "").child("listingId").setValue(id);
-                    mDatabase.child("UserCart").child(email).child(id + "").child("cartQty")
-                            .setValue(Integer.parseInt(holder.homequantity.getText().toString()));
-                    Toast.makeText(mContext, "Your item has been succesfully added", Toast.LENGTH_SHORT).show();
-                }
+                String email = mAuth.getCurrentUser().getEmail().replace("@", "")
+                        .replace(".", "");
+                int id = mListingId.get(position);
+                mDatabase.child("UserCart").child(email).child(id+"").child("listingId").setValue(id);
+                mDatabase.child("UserCart").child(email).child(id+"").child("cartQty")
+                        .setValue(Integer.parseInt(holder.homequantity.getText().toString()));
             }
         });
 
@@ -238,8 +203,6 @@ public class StaggeredRecyclerViewAdapter extends RecyclerView.Adapter<Staggered
         ImageView image;
         TextView name, price, category, description, quantity, discount;
         Button addtoCart;
-        TextView desc;
-        CardView container;
         EditText homequantity;
         Button plus;
         Button minus;
@@ -250,8 +213,6 @@ public class StaggeredRecyclerViewAdapter extends RecyclerView.Adapter<Staggered
             this.image = itemView.findViewById(R.id.imageview_widget);
             this.name = itemView.findViewById(R.id.name_widget);
             this.addtoCart = itemView.findViewById(R.id.addtoCart);
-            desc = itemView.findViewById(R.id.textView19);
-            container = itemView.findViewById(R.id.chomecard);
             this.homequantity = itemView.findViewById(R.id.homequantity);
             this.plus = itemView.findViewById(R.id.plus2);
             this.minus = itemView.findViewById(R.id.minus2);
@@ -259,17 +220,6 @@ public class StaggeredRecyclerViewAdapter extends RecyclerView.Adapter<Staggered
             this.description = itemView.findViewById(R.id.description_text);
             this.discount = itemView.findViewById(R.id.disc_widget);
             this.quantity = itemView.findViewById(R.id.qty_widget);
-
-            container.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-
-                    Listing list = Listing.get(getAdapterPosition());
-                    list.setExpanded(!list.isExpanded());
-                    notifyItemChanged(getAdapterPosition());
-                }
-            });
-
         }
     }
 }
